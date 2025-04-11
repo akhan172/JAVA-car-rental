b1d17633ec1cfeaced79074549362bef46a5fff3
Abrar <abrkhn731@gmail.com>
2025-04-11 22:53:07 +0530

    feat: made all the remaining classes but the classes are incomplete

---
+++ src/Customer.java
@@ -0,0 +1,18 @@
+
+class Customer {
+ private int cusId;
+ private String cusName;
+ 
+ Customer(int cusId, String cusName){
+	 this.cusId=cusId;
+	 this.cusName=cusName;
+ }
+ 
+ public int getCusId() {
+	return cusId;
+}
+
+ public String getCusName() {
+	return cusName;
+}
+}
