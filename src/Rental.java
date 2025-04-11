90ac8ec5e415b9999e508b285cfdccf04a5b5b27
Your Name <you@example.com>
2025-04-11 23:06:10 +0530

    completed the rental class file

--- src/Rental.java
+++ src/Rental.java
@@ -1,4 +1,25 @@
 
 class Rental {
-
+	private Car car;
+	private Customer cus;
+	private int days;
+	
+	Rental(Car car, Customer cus, int days){
+		this.car=car;
+		this.cus=cus;
+		this.days=days;
+	}
+	
+	public Car getCar() {
+		return car;
+	}
+	
+	public Customer getCus() {
+		return cus;
+	}
+	
+	public int getDays() {
+		return days;
+	}
+	
 }
